2054b3a3e421e373e24ad6e251685cc128170a81
HemangVora <hemangvora2@gmail.com>
2023-01-11 18:51:12 +0530

    react project initialized

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import { Route, Router, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <>
+
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default App;
